5dbf9687fc79e50b62186c22a0743656ff60e6e4
makairasoft <jmarlin@makairasoftware.com>
2024-06-21 13:36:35 -0400

    #3 this copy of the boot ROM successfully and repeatably reads standard-formatted Apple II disks

--- bios.asm
+++ bios.asm
@@ -205,7 +205,7 @@ TILE_LOAD_LOOP:
 STACK         =    $0100  
 TWOS_BUFFER   =     $0300    ;holds the 2-bit chunks
 CONV_TAB      =     $0356   ;6+2 conversion table
-BOOT1         =     $0800   ;buffer for next stage of loader
+BOOT1         =     $0400   ;buffer for next stage of loader
 IWM_PH0_OFF   =     $c080             ;stepper motor control
 IWM_PH0_ON    =     $c081             ;stepper motor control
 IWM_MOTOR_ON  =     $c089             ;starts drive spinning
@@ -213,22 +213,94 @@ IWM_MOTOR_OFF =     $c088
 IWM_SEL_DRIVE_1 =   $c08a             ;selects drive 1
 IWM_Q6_OFF    =     $c08c             ;read
 IWM_Q7_OFF    =     $c08e             ;WP sense/read
-MON_WAIT      =     $fca8             ;delay for (26 + 27*Acc + 5*(Acc*Acc))/2 cycles
-MON_IORTS     =     $ff58             ;JSR here to find out where one is
 
-found_track = $40
-bits = $3c
+data_ptr      =    $26       ;pointer to BOOT1 data buffer
+slot_index    =    $2b       ;slot number << 4
+bits          =    $3c       ;temp storage for bit manipulation
+sector        =    $3d       ;sector to read
+found_track   =    $40       ;track found
+track         =    $41       ;track to read
 
 lda #'X'
 .GLOBAL PRNTCHR
 jsr PRNTCHR
 
+;Specify load from track 0, sector 0
+lda #$00
+sta track
+sta sector
 
 ldx     #$60
 lda     IWM_Q7_OFF,x
 lda     IWM_Q6_OFF,x
 lda     IWM_SEL_DRIVE_1,x
 lda     IWM_MOTOR_ON,x
+
+    ldy     #$00
+    sty track
+    sty sector
+    ldx     #$03
+CreateDecTabLoop:
+    stx     bits
+    txa
+    asl     A                 ;shift left, putting high bit in carry
+    bit     bits              ;does shifted version overlap?
+    beq     reject           ;no, doesn't have two adjacent 1s
+    ora     bits              ;merge
+    eor     #$ff              ;invert
+    and     #$7e              ;clear hi and lo bits
+check_dub0:
+    bcs     reject           ;initial hi bit set *or* adjacent 0 bits set
+    lsr     A                 ;shift right, low bit into carry
+    bne     check_dub0       ;if more bits in byte, loop
+    tya                       ;we have a winner... store Y-reg to memory
+    sta     CONV_TAB,x        ;actual lookup will be on bytes with hi bit set
+    iny                       ; so they'll read from CONV_TAB-128
+reject:
+    inx                       ;try next candidate
+    bpl     CreateDecTabLoop
+; 
+; Prep the hardware.
+; 
+    ;jsr     MON_IORTS         ;known RTS
+    ;tsx
+    ;lda     STACK,x           ;pull hi byte of our address off stack
+    ;asl     A                 ;(we assume no interrupts have hit)
+    ;asl     A                 ;multiply by 16
+    ;asl     A
+    ;asl     A
+    lda #$60
+    sta     slot_index        ;keep this around
+    tax
+    lda     IWM_Q7_OFF,x      ;set to read mode
+    lda     IWM_Q6_OFF,x
+    lda     IWM_SEL_DRIVE_1,x ;select drive 1
+    lda     IWM_MOTOR_ON,x    ;spin it up
+
+; 
+; Blind-seek to track 0.
+; 
+    ldy     #80               ;80 phases (40 tracks)
+seek_loop:
+    lda     IWM_PH0_OFF,x     ;turn phase N off
+    tya
+    and     #$03              ;mod the phase number to get 0-3
+    asl     A                 ;double it to 0/2/4/6
+    ora     slot_index        ;add in the slot index
+    tax
+    lda     IWM_PH0_ON,x      ;turn on phase 0, 1, 2, or 3
+    lda     #86
+    jsr     MON_WAIT          ;wait 19664 cycles
+    dey                       ;next phase
+    bpl     seek_loop
+
+    lda #$00
+    sta     data_ptr          ;A-reg is 0 when MON_WAIT returns
+    sta     sector            ;so we're looking for T=0 S=0
+    sta     track
+    lda     #>BOOT1           ;write the output here
+    sta     data_ptr+1
+
 
 ReadSector:   clc
 ReadSector_C: php
@@ -239,12 +311,10 @@ check_d5:     eor #$d5
               nop
               nop
               nop
-              nop
 rdbyte2:      lda IWM_Q6_OFF,x
               bpl rdbyte2
               cmp #$aa
               bne check_d5
-              nop
               nop
               nop
               nop
@@ -260,10 +330,10 @@ rdbyte3:      lda IWM_Q6_OFF,x
               bne ReadSector
 
 FoundAddress:
+
     ldy #$03
 hdr_loop:
     sta found_track
-    nop
     nop
     nop
     nop
@@ -276,7 +346,7 @@ adr_hdr_rdbyte1:
     nop
     nop
     nop
-adr_hdr_rdbyte2:
+ adr_hdr_rdbyte2:
     lda IWM_Q6_OFF,x
     bpl adr_hdr_rdbyte2
     and bits
@@ -289,10 +359,67 @@ adr_hdr_rdbyte2:
     cmp track
     bne ReadSector
     bcs ReadSector_C
-JMP init
 
 FoundData:
-    beq ReadSector
+    ldy #86
+read_twos_loop:
+    sty bits
+dat_twos_rdbyte1:
+    ldy IWM_Q6_OFF,x ;ldy IWM_Q6_OFF,x
+    bpl dat_twos_rdbyte1
+    eor $02d6,y
+    ldy bits
+    dey
+    sta TWOS_BUFFER,y
+    nop
+    nop
+    nop ;Possibly here is good
+    bne read_twos_loop
+
+read_sixes_loop:
+    sty bits
+sixes_rdbyte2:
+    ldy IWM_Q6_OFF,x
+    bpl sixes_rdbyte2
+    eor CONV_TAB-128,y
+    ldy bits
+    sta (data_ptr),y
+    iny
+    nop
+    nop
+    nop
+    nop
+    ;nop
+    ;nop ;Possibly here is good
+    bne read_sixes_loop
+
+checksum_rdbyte3:
+    ldy IWM_Q6_OFF,x
+    bpl checksum_rdbyte3
+    eor CONV_TAB-128,y
+another:
+    beq Decode
+    jmp ReadSector
+
+; 
+; Decode the 6+2 encoding.  The high 6 bits of each byte are in place, now we
+; just need to shift the low 2 bits of each in.
+; 
+Decode:
+    ldy     #$00              ;update 256 bytes
+init_x:
+    ldx     #86               ;run through the 2-bit pieces 3x (86*3=258)
+decode_loop:
+    dex
+    bmi     init_x           ;if we hit $2ff, go back to $355
+    lda     (data_ptr),y      ;foreach byte in the data buffer...
+    lsr     TWOS_BUFFER,x     ; grab the low two bits from the stuff at $300-$355
+    rol     A                 ; and roll them into the low two bits of the byte
+    lsr     TWOS_BUFFER,x
+    rol     A
+    sta     (data_ptr),y
+    iny
+    bne     decode_loop
 
 DiskTestDone:
     lda IWM_MOTOR_OFF,x
@@ -300,6 +427,19 @@ DiskTestDone:
     JMP init
 HALT:
     JMP HALT
+
+MON_WAIT:
+    TYA
+    LDY #$01
+MON_TOP:
+    BIT $2002
+    BPL MON_TOP
+    DEY
+    BNE MON_TOP
+    TAY
+    LDA #$00
+    RTS
+    
 
 IRQ_BRK_HANDLE:
     RTI
