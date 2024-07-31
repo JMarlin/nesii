2ce54c3464d3cd4d2a8a8873db36abaa0388fbcd
makairasoft <jmarlin@makairasoftware.com>
2024-07-31 12:12:59 -0400

    #23 cleanup of char_io module, has not been checked for compilability

--- bios/bios.asm
+++ bios/bios.asm
@@ -1,49 +1,35 @@
 .segment "CODE"
 .include "char_io.inc"
 .include "monitor.inc"
-.include "rom_floppy_constants.inc"
 .include "startup_interface.inc"
 .include "floppy.inc"
 .include "globals.inc"
 
-;I should probably make a note here, since I cleaned up the sources a bunch,
-;that this file is an absolute mess and should be broken up
-;If you're wondering why, it's because this file was the origin of the
-;whole codebase and as such doesn't really serve much purposes now
-;aside from being the entry point and a general bucket of old but critical stuff
-
-ENTRY:
+entry:
 ;Turn off interrupts and decimal mode
-    SEI
-    CLD
-    LDX #$FF
-    TXS
+    sei
+    cld
+    ldx #$FF
+    txs
 
 ;Initialize PPU state (disable interrupts and rendering)
-    LDA #$00
-    STA $2000
-    STA $2001
+    lda #$00
+    sta $2000
+    sta $2001
 
 ;Wait for three vblanks (let PPU state settle?)
-    BIT $2002
+    bit $2002
 ppu_vblank_wait1:
-    BIT $2002
-    BPL ppu_vblank_wait1
-
-    BIT $2002
+    bit $2002
+    bpl ppu_vblank_wait1
+    bit $2002
 ppu_vblank_wait2:
-    BIT $2002
-    BPL ppu_vblank_wait2
-
-    BIT $2002
+    bit $2002
+    bpl ppu_vblank_wait2
+    bit $2002
 ppu_vblank_wait3:
-    BIT $2002
-    BPL ppu_vblank_wait3
-
-;Wait for a vblank
-:
-    BIT $2002
-    BPL :-
+    bit $2002
+    bpl ppu_vblank_wait3
 
 ;Run the BIOS out of RAM to allow for BIOS updates
 ;Copy ourself from E000-FFFF to A000-BFFF
@@ -72,164 +58,122 @@ bne copy_next_bios_byte
 lda #$02
 sta $d000
 
-RAWF=139
-
+rawf=139
+;Initialize the APU so we can make a debug beep
 init_apu:
-        ; Init $4000-4013
+        ; Copy values into APU registers
         ldy #$13
-@loop:  lda regs,y
+@loop:  lda apu_regs,y
         sta $4000,y
         dey
         bpl @loop
- 
         ; We have to skip over $4014 (OAMDMA)
         lda #$0f
         sta $4015
         lda #$40
         sta $4017
-
-
-        lda #<RAWF
+        ;Turn on a channel for a boot-up beep
+        lda #<rawf
         sta $4002
-        lda #>RAWF
+        lda #>rawf
         sta $4003
         lda #%10111111
         sta $4000
 
 ;Set background palette values
 ;Universal background = black
-    LDA #$3F
-    STA $2006
-    LDA #$00
-    STA $2006
-    LDA #$20
-    STA $2007
-
-    LDA #$3F
-    STA $2006
-    LDA #$01
-    STA $2006
-    LDA #$20
-    STA $2007
-
-    LDA #$3F
-    STA $2006
-    LDA #$02
-    STA $2006
-    LDA #$1D
-    STA $2007
-    
-    LDA #$3F
-    STA $2006
-    LDA #$03
-    STA $2006
-    LDA #$1D
-    STA $2007
+    lda #$3f
+    sta $2006
+    lda #$00
+    sta $2006
+    lda #$20
+    sta $2007
+;Palette 1
+    lda #$3f
+    sta $2006
+    lda #$01
+    sta $2006
+    lda #$20
+    sta $2007
+;Palette 2
+    lda #$3f
+    sta $2006
+    lda #$02
+    sta $2006
+    lda #$1d
+    sta $2007
+;Palette 3
+    lda #$3f
+    sta $2006
+    lda #$03
+    sta $2006
+    lda #$1d
+    sta $2007
 
 ;Fill attribute tables to all be palette zero
-    LDX #128
-WRITE_ATTR_TOP:
-    LDA #$23
-    STA $2006
-    TXA
-    CLC
-    ADC #$BF
-    STA $2006
-    LDA #$00
-    STA $2007
-    DEX
-    BNE WRITE_ATTR_TOP
+    ldx #128
+write_attr_top:
+    lda #$23
+    sta $2006
+    txa
+    clc
+    adc #$BF
+    sta $2006
+    lda #$00
+    sta $2007
+    dex
+    bne write_attr_top
 
 ;Wait for VBLANK
 :
-    BIT $2002
-    BPL :-
-
+    bit $2002
+    bpl :-
 ;Fill the screen with blank tiles
-    LDA #$20
-    STA $2006
-    LDA #$00
-    STA $2006
-    LDX #$3C
-    LDA #$3F
+    lda #$20
+    sta $2006
+    lda #$00
+    sta $2006
+    ldx #$3C
+    lda #$3F
 :
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    STA $2007
-    DEX
-    BNE :-
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+    dex
+    bne :-
 
-;Load tileset into pattern RAM
-.GLOBAL CHAR_TILES
-TILE_PTR=$FE
-    LDA #<CHAR_TILES
-    STA TILE_PTR
-    LDA #>CHAR_TILES
-    STA TILE_PTR+1
+;Initialize text mode
+    jsr char_io_init_screen
 
-    LDY #0
-    STY $2001
-    STY $2006
-    STY $2006
-    LDX #4
-
-TILE_LOAD_LOOP:
-    LDA (TILE_PTR),Y
-    STA $2007
-    INY
-    BNE TILE_LOAD_LOOP
-    INC TILE_PTR+1
-    DEX
-    BNE TILE_LOAD_LOOP
-
-;Set scroll to 0, 0
-    LDA #$00
-    STA $2005
-    STA $2005
-    STA SCROLL_VALUE
-
-;Initialize print location to 1,1 on the playfield
-    LDA #$21
-    STA SCREEN_ADDR
-    LDA #$20
-    STA SCREEN_ADDR+1
-
-;When we first start, we do not yet need to scroll
-    LDA #$00
-    STA IS_SCROLLING
-
-;Enable rendering
-    LDA #$0E
-    STA $2001
-
-    LDA #$00
-    STA $4000
-
-lda #<BOOT_MSG
-sta $03
-lda #>BOOT_MSG
-sta $04
-jsr PRINTSTR
-
+;Turn off beep
+    lda #$00
+    sta $4000
+;Print startup banner
+    lda #<boot_msg
+    sta $03
+    lda #>boot_msg
+    sta $04
+    jsr char_io_printstr
+;Init floppy and try to boot
     jsr floppy_init
     jsr system_startup
     jmp init
 
-.global MON_WAIT
-MON_WAIT:
+.global mon_wait
+mon_wait:
     lda #$f0
     sta $da
 wait_loop_b:
@@ -247,110 +191,107 @@ wait_loop_a:
 step_track:
     lda     cur_track
     and     #$01
-    asl     A
-    asl     A
+    asl     
+    asl     
     ora     #$62
     tax
-    lda     IWM_PH0_ON,x      ;turn on phase 0, 1, 2, or 3
+    lda     iwm_ph0_on,x      ;turn on phase 0, 1, 2, or 3
     lda     #86
-    jsr     MON_WAIT          ;wait 19664 cycles
-    lda     IWM_PH0_OFF,x     ;turn phase N off
+    jsr     mon_wait          ;wait 19664 cycles
+    lda     iwm_ph0_off,x     ;turn phase N off
     inx
     inx
     txa
-    and     #$F7
+    and     #$f7
     tax
-    lda     IWM_PH0_ON,x
+    lda     iwm_ph0_on,x
     lda     #86
-    jsr     MON_WAIT
-    lda     IWM_PH0_OFF,x
+    jsr     mon_wait
+    lda     iwm_ph0_off,x
     inc     cur_track
     rts
 
+.global load_next_sector
 load_next_sector:
-.GLOBAL load_next_sector
-    lda     cur_sector
-    ldx     cur_track
-    jsr     floppy_read
+    lda cur_sector
+    ldx cur_track
+    jsr floppy_read
     lda r15
     bne next_sector_done
-    inc     cur_sector
-    lda     cur_sector
-    cmp     #$10
-    bne     next_sector_done
-    lda     #$00
-    sta     cur_sector
-    jsr     step_track
+    inc cur_sector
+    lda cur_sector
+    cmp #$10
+    bne next_sector_done
+    lda #$00
+    sta cur_sector
+    jsr step_track
 next_sector_done:
     rts
 
 .global load_boot_sector
 load_boot_sector:
-    lda     #$00
-    sta     data_ptr          ;Store page-aligned
-    lda     #>BOOT1           ;Target is the NES RAM boot area
-    sta     data_ptr+1
-    jsr     load_next_sector
+    lda #$00
+    sta data_ptr          ;Store page-aligned
+    lda #>boot1           ;Target is the NES RAM boot area
+    sta data_ptr+1
+    jsr load_next_sector
     lda r15
     beq load_boot_sector_exit
     jsr floppy_off
-    lda #<READ_ERROR_MSG
+    lda #<read_error_message
     sta string_ptr
-    lda #>READ_ERROR_MSG
+    lda #>read_error_message
     sta string_ptr+1
-    jsr PRINTSTR
+    jsr char_io_printstr
     jmp init
 load_boot_sector_exit:
     rts
     
 BOOT_MSG:
-    .ASCIIZ "LOADING..."
+    .asciiz "LOADING..."
 
-READ_ERROR_MSG:
-    .ASCIIZ "READ ERROR"
+read_error_message:
+    .asciiz "READ ERROR"
 
 IRQ_BRK_HANDLE:
-    RTI
+    rti
    
-regs:
+apu_regs:
     .byte $30,$08,$00,$00
     .byte $30,$08,$00,$00
     .byte $80,$00,$00,$00
     .byte $30,$00,$00,$00
     .byte $00,$00,$00,$00
 
-CHAR_TILES:
-    .incbin "blob/font.chr"
-
 .segment "CALL_TABLE"
-jsr PRINTSTR         ;FFC0
+jsr char_io_printstr          ;FFC0
 rts
-jsr PRNTCHR          ;FFC4
+jsr char_io_printchr          ;FFC4
 rts
-jsr INITKEYBOARD     ;FFC8 
+jsr char_io_init_keyboard     ;FFC8 
 rts
-jsr load_next_sector ;FFCC
+jsr load_next_sector          ;FFCC
 rts
-jmp init             ;FFD0
+jmp init                      ;FFD0
 rts
-jsr read_sector      ;FFD4
+jsr read_sector               ;FFD4
 rts
-jsr MON_WAIT         ;FFD8
+jsr mon_wait                  ;FFD8
 rts
-jsr GETKEY           ;FFDC
+jsr char_io_getkey            ;FFDC
 rts
-jsr floppy_read     ;FFE0
+jsr floppy_read               ;FFE0
 rts
-jsr floppy_init     ;FFE4
+jsr floppy_init               ;FFE4
 rts
-jsr floppy_off      ;FFE8
+jsr floppy_off                ;FFE8
 rts
-jsr floppy_on       ;FFEC
+jsr floppy_on                 ;FFEC
 rts
-.byte "NES" ;FFF0
+.byte "NES"                   ;FFF0
 
 .segment "VECTORS"
 .word $0000
-.word ENTRY
-.word IRQ_BRK_HANDLE
+.word entry
+.word irq_brk_handle
 
--- bios/char_io.asm
+++ bios/char_io.asm
@@ -1,409 +1,442 @@
-.SEGMENT "CODE"
-.INCLUDE "char_io.inc"
+.segment "CODE"
+.include "char_io.inc"
 
-INITKEYBOARD:
-.GLOBAL INITKEYBOARD
+
+.global char_io_init_screen
+char_io_init_screen:
+;Initialize pointer into tile table
+tile_ptr=$fe
+    lda #<char_tiles
+    sta tile_ptr
+    lda #>char_tiles
+    sta tile_ptr+1
+;Initialize PPU write counter
+    ldy #0
+    sty $2001
+    sty $2006
+    sty $2006
+    ldx #4
+tile_load_loop:
+    lda (tile_ptr),Y
+    sta $2007
+    iny
+    bne tile_load_loop
+    inc tile_ptr+1
+    dex
+    bne tile_load_loop
+;Set scroll to 0, 0
+    lda #$00
+    sta $2005
+    sta $2005
+    sta scroll_value
+;Initialize print location to 1,1 on the playfield
+    lda #$21
+    sta screen_addr
+    lda #$20
+    sta screen_addr+1
+;When we first start, we do not yet need to scroll
+    lda #$00
+    sta is_scrolling
+;Enable rendering
+    lda #$0e
+    sta $2001
+    rts
+
+
+char_io_init_keyboard:
+.global char_io_init_keyboard
     ;Loop over each column and check the 7th row to see if we are at column 1
-KB_INIT_COLUMN_LOOP:
-    LDA #$01
-    STA $4016
-    LDA $4017
-    LDA #$00
-    STA $4016
-    LDY #$06
-KB_ROW_SKIP_LOOP:
-    LDA $4017
-    DEY
-    BNE KB_ROW_SKIP_LOOP
-    AND #$01
-    BNE KB_INIT_COLUMN_LOOP
-KB_COL_ONE_FOUND:
-    RTS
+keyboard_init_column_loop:
+    lda #$01
+    sta $4016
+    lda $4017
+    lda #$00
+    sta $4016
+    ldy #$06
+keyboard_row_skip_loop:
+    lda $4017
+    dey
+    bne keyboard_row_skip_loop
+    and #$01
+    bne keyboard_init_column_loop
+keyboard_column_one_found:
+    rts
 
-GETKEY:
-.GLOBAL GETKEY
-    LDA #0
-    STA KEY_WAS_FOUND
-    LDX #0
-NEXT_KB_COLUMN_LOOP:
-    LDY #6
-    LDA #01
-CLK_KB_BIT_LOOP:
-    STA $4016
-    LDA $4017
-    STA LAST_KB_BIT
-    LDA KEY_WAS_FOUND
-    BNE CONTINUE_KB_BIT_LOOP
-    LDA LAST_KB_BIT
-    AND #$01
-    BNE CONTINUE_KB_BIT_LOOP
-    LDA KEY_LUT,X
-    PHA 
-    LDA #$01
-    STA KEY_WAS_FOUND
-    CLC
-    BCC CONTINUE_KB_BIT_LOOP
-DO_ANOTHER_INNER_KB_LOOP:
-    LDA #$00
-    CLC
-    BCC CLK_KB_BIT_LOOP
-CONTINUE_KB_BIT_LOOP:
-    INX
-    DEY
-    BNE DO_ANOTHER_INNER_KB_LOOP
-    TXA
-    CMP #48
-    BNE NEXT_KB_COLUMN_LOOP
-    LDA KEY_WAS_FOUND
-    BNE RETURN_STACK_KEY_VALUE
-    LDA #$FF
-    STA LAST_PRESSED_KEY
-    CLC
-    BCC GETKEY
-RETURN_STACK_KEY_VALUE:
-    PLA
-    CMP LAST_PRESSED_KEY
-    BEQ GETKEY
-    STA LAST_PRESSED_KEY
-    RTS
 
-;PRINT CHARACTER SUBROUTINE
+.global char_io_getkey
+char_io_getkey:
+    lda #0
+    sta key_was_found
+    ldx #0
+next_keyboard_column_loop:
+    ldy #6
+    lda #01
+clock_keyboard_bit_loop:
+    sta $4016
+    lda $4017
+    sta last_keyboard_bit
+    lda key_was_found
+    bne continue_keyboard_bit_loop
+    lda last_keyboard_bit
+    and #$01
+    bne continue_keyboard_bit_loop
+    lda key_lut,x
+    pha 
+    lda #$01
+    sta key_was_found
+    clc
+    bcc continue_keyboard_bit_loop
+do_another_inner_keyboard_loop:
+    lda #$00
+    clc
+    bcc clock_keyboard_bit_loop
+continue_keyboard_bit_loop:
+    inx
+    dey
+    bne do_another_inner_keyboard_loop
+    txa
+    cmp #48
+    bne next_keyboard_column_loop
+    lda key_was_found
+    bne return_stack_key_value
+    lda #$FF
+    sta last_pressed_key
+    clc
+    bcc char_io_getkey
+return_stack_key_value:
+    pla
+    cmp last_pressed_key
+    beq char_io_getkey
+    sta last_pressed_key
+    rts
+
+
+char_io_printchr:
+.global char_io_printchr
 ;Wait for VBLANK
-PRNTCHR:
-.GLOBAL PRNTCHR
 :
-    BIT $2002
-    BPL :-
-
+    bit $2002
+    bpl :-
     ;Look up the tile number of this character, print nothing if it was zero
-    TAX
-    LDA CHR_LUT,X
-    CMP #$FF
-    BEQ CHECK_CTL
-
+    tax
+    lda chr_lut,X
+    cmp #$FF
+    beq check_ctl
     ;Non-zero tile number found, write it to the PPU and increment the cursor
-    LDY SCREEN_ADDR+1
-    STY $2006
-    LDX SCREEN_ADDR
-    STX $2006
-    STA $2007
-
+    ldy screen_addr+1
+    sty $2006
+    ldx screen_addr
+    stx $2006
+    sta $2007
     ;Character was successfully printed, so advance the cursor
-STEP_CHARACTER:
-    INX
-    STX SCREEN_ADDR
-    BNE CHECK_END_OF_SREEN
-    INY
-    STY SCREEN_ADDR+1
-CHECK_END_OF_SREEN:
-    TXA
-    AND #$1F
-    CMP #$1F
-    BNE PRNTCHR_END
-STEP_OVER_MARGIN:
-    INX
-    BNE KEEP_ON_STEPPIN
-    INY
-    STY SCREEN_ADDR+1
-KEEP_ON_STEPPIN:   
-    INX
-    STX SCREEN_ADDR
-DO_WRAP_SCROLL:
-    TXA
-    AND #$E0
-    JSR CHECK_AND_SCROLL
-    CLC
-    BCC PRNTCHR_END
+step_character:
+    inx
+    stx screen_addr
+    bne check_end_of_screen
+    iny
+    sty screen_addr+1
+check_end_of_screen:
+    txa
+    and #$1f
+    cmp #$1f
+    bne char_io_printchr_end
+step_over_margin:
+    inx
+    bne keep_on_steppin
+    iny
+    sty screen_addr+1
+keep_on_steppin:   
+    inx
+    stx screen_addr
+do_wrap_scroll:
+    txa
+    and #$e0
+    jsr check_and_scroll
+    clc
+    bcc char_io_printchr_end
+check_ctl:
+    txa
+    cmp #$0d
+    bne check_linefeed
+    lda screen_addr
+    and #$e0
+    tax
+    inx
+    txa
+    sta screen_addr
+    clc
+    bcc char_io_printchr_end
+check_linefeed:
+    cmp #$0a
+    bne char_io_printchr_end
+    lda screen_addr
+    clc
+    adc #$20
+    sta screen_addr
+    and #$e0
+    bne do_lf_scroll
+    ldx screen_addr+1
+    inx
+    stx screen_addr+1
+do_lf_scroll:
+    jsr check_and_scroll
+char_io_printchr_end:
+    lda #$00
+    sta $2005
+    lda scroll_value
+    sta $2005
+    rts
 
-CHECK_CTL:
-    TXA
-    CMP #$0D
-    BNE CHECK_LINEFEED
-    LDA SCREEN_ADDR
-    AND #$E0
-    TAX
-    INX
-    TXA
-    STA SCREEN_ADDR
-    CLC
-    BCC PRNTCHR_END
 
-CHECK_LINEFEED:
-    CMP #$0A
-    BNE PRNTCHR_END
-    LDA SCREEN_ADDR
-    CLC
-    ADC #$20
-    STA SCREEN_ADDR
-    AND #$E0
-    BNE DO_LF_SCROLL
-    LDX SCREEN_ADDR+1
-    INX
-    STX SCREEN_ADDR+1
-
-DO_LF_SCROLL:
-    JSR CHECK_AND_SCROLL
-
-PRNTCHR_END:
-    LDA #$00
-    STA $2005
-    LDA SCROLL_VALUE
-    STA $2005
-
-    RTS
-
-;PRINT STRING SUBROUTINE
-.GLOBAL PRINTSTR
-PRINTSTR:
-.GLOBAL PRINTSTR
-    LDY #$00
+.global char_io_printstr
+char_io_printstr:
+.global char_io_printstr
+    ldy #$00
 :
-    LDA (STRING_PTR),Y
-    BEQ PRINTSTR_END
-    TAX
-    TYA
-    PHA
-    TXA
-    JSR PRNTCHR
-    PLA
-    TAY
-    INY
-    CLC
-    BCC :-
-PRINTSTR_END:
-    RTS
+    lda (string_pointer),y
+    beq char_io_printstr_end
+    tax
+    tya
+    pha
+    txa
+    jsr char_io_printchr
+    pla
+    tay
+    iny
+    clc
+    bcc :-
+char_io_printstr_end:
+    rts
 
-;SUBROUTINE FOR SOFTWARE LINE SCROLLING
-CHECK_AND_SCROLL:
-    TAX
-    LDA SCREEN_ADDR+1
-    CMP #$23
-    BNE CHECK_SCROLL_FLAG
-    TXA
-    CMP #$C0
-    BNE CHECK_SCROLL_FLAG
 
-    LDA #$01
-    STA IS_SCROLLING
-
+check_and_scroll:
+    tax
+    lda screen_addr+1
+    cmp #$23
+    bne check_scroll_flag
+    txa
+    cmp #$C0
+    bne check_scroll_flag
+    lda #$01
+    sta is_scrolling
     ;If we are at the end of the nametable, wrap to the beginning
-    LDA SCREEN_ADDR
-    AND #$1F
-    STA SCREEN_ADDR
-    LDA #$20
-    STA SCREEN_ADDR+1
-    LDA #$01
+    lda screen_addr
+    and #$1f
+    sta screen_addr
+    lda #$20
+    sta screen_addr+1
+    lda #$01
+check_scroll_flag:
+    lda is_scrolling
+    beq char_io_printchr_end
+do_screen_scroll:
+    lda screen_addr+1
+    sta $2006
+    lda screen_addr
+    and #$e0
+    sta $2006
+    ldx #$20
+    lda #$3f
+line_clear_top:
+    sta $2007
+    dex
+    bne line_clear_top
+    lda scroll_value
+    clc
+    adc #$08
+    cmp #$f0
+    beq wrap_scroll
+    sta scroll_value
+    rts
+wrap_scroll:
+    lda #$00
+    sta scroll_value
+    rts
 
-CHECK_SCROLL_FLAG:
-    LDA IS_SCROLLING
-    BEQ PRNTCHR_END
 
-DO_SCREEN_SCROLL:
-    LDA SCREEN_ADDR+1
-    STA $2006
-    LDA SCREEN_ADDR
-    AND #$E0
-    STA $2006
-    LDX #$20
-    LDA #$3F
-LINE_CLEAR_TOP:
-    STA $2007
-    DEX
-    BNE LINE_CLEAR_TOP
+char_tiles:
+    .incbin "blob/font.chr"
+    
 
-    LDA SCROLL_VALUE
-    CLC
-    ADC #$08
-    CMP #$F0
-    BEQ WRAP_SCROLL
-    STA SCROLL_VALUE
+.segment "KEY_LUT"
+key_lut:
+.byte ';'
+.byte 'L'
+.byte 'K'
+.byte 'J'
+.byte 'H'
+.byte $20
+.byte 'P'
+.byte 'O'
+.byte 'I'
+.byte 'U'
+.byte 'Y'
+.byte $0D
+.byte '0'
+.byte '9'
+.byte '8'
+.byte '7'
+.byte '6'
+.byte $00
+.byte 'Z'
+.byte 'X'
+.byte 'C'
+.byte 'V'
+.byte 'B'
+.byte $00
+.byte 'A'
+.byte 'S'
+.byte 'D'
+.byte 'F'
+.byte 'G'
+.byte $00
+.byte 'Q'
+.byte 'W'
+.byte 'E'
+.byte 'R'
+.byte 'T'
+.byte $00
+.byte '1'
+.byte '2'
+.byte '3'
+.byte '4'
+.byte '5'
+.byte $00
+.byte '/'
+.byte '.'
+.byte ','
+.byte 'M'
+.byte 'N'
+.byte '+'
 
-    RTS
 
-WRAP_SCROLL:
-    LDA #$00
-    STA SCROLL_VALUE
-
-    RTS
-
-.SEGMENT "KEY_LUT"
-KEY_LUT:
-.BYTE ';'
-.BYTE 'L'
-.BYTE 'K'
-.BYTE 'J'
-.BYTE 'H'
-.BYTE $20
-.BYTE 'P'
-.BYTE 'O'
-.BYTE 'I'
-.BYTE 'U'
-.BYTE 'Y'
-.BYTE $0D
-.BYTE '0'
-.BYTE '9'
-.BYTE '8'
-.BYTE '7'
-.BYTE '6'
-.BYTE $00
-.BYTE 'Z'
-.BYTE 'X'
-.BYTE 'C'
-.BYTE 'V'
-.BYTE 'B'
-.BYTE $00
-.BYTE 'A'
-.BYTE 'S'
-.BYTE 'D'
-.BYTE 'F'
-.BYTE 'G'
-.BYTE $00
-.BYTE 'Q'
-.BYTE 'W'
-.BYTE 'E'
-.BYTE 'R'
-.BYTE 'T'
-.BYTE $00
-.BYTE '1'
-.BYTE '2'
-.BYTE '3'
-.BYTE '4'
-.BYTE '5'
-.BYTE $00
-.BYTE '/'
-.BYTE '.'
-.BYTE ','
-.BYTE 'M'
-.BYTE 'N'
-.BYTE '+'
-
-.SEGMENT "CHR_LUT"
-CHR_LUT:
-.BYTE $FF ; 0x0 
-.BYTE $FF ; 0x1
-.BYTE $FF ; 0x2
-.BYTE $FF ; 0x3
-.BYTE $FF ; 0x4
-.BYTE $FF ; 0x5
-.BYTE $FF ; 0x6
-.BYTE $FF ; 0x7
-.BYTE $FF ; 0x8
-.BYTE $FF ; 0x9
-.BYTE $FF ; 0xa
-.BYTE $FF ; 0xb
-.BYTE $FF ; 0xc
-.BYTE $FF ; 0xd
-.BYTE $FF ; 0xe
-.BYTE $FF ; 0xf
-.BYTE $FF ; 0x10
-.BYTE $FF ; 0x11
-.BYTE $FF ; 0x12
-.BYTE $FF ; 0x13
-.BYTE $FF ; 0x14
-.BYTE $FF ; 0x15
-.BYTE $FF ; 0x16
-.BYTE $FF ; 0x17
-.BYTE $FF ; 0x18
-.BYTE $FF ; 0x19
-.BYTE $FF ; 0x1a
-.BYTE $FF ; 0x1b
-.BYTE $FF ; 0x1c
-.BYTE $FF ; 0x1d
-.BYTE $FF ; 0x1e
-.BYTE $FF ; 0x1f
-.BYTE $3F ; 0x20
-.BYTE $24 ; 0x21
-.BYTE $2D ; 0x22
-.BYTE $27 ; 0x23
-.BYTE $25 ; 0x24
-.BYTE $2E ; 0x25
-.BYTE $2F ; 0x26
-.BYTE $29 ; 0x27
-.BYTE $30 ; 0x28
-.BYTE $31 ; 0x29
-.BYTE $32 ; 0x2a
-.BYTE $33 ; 0x2b
-.BYTE $2A ; 0x2c
-.BYTE $27 ; 0x2d
-.BYTE $28 ; 0x2e
-.BYTE $34 ; 0x2f
-.BYTE $00 ; 0x30
-.BYTE $01 ; 0x31
-.BYTE $02 ; 0x32
-.BYTE $03 ; 0x33
-.BYTE $04 ; 0x34
-.BYTE $05 ; 0x35
-.BYTE $06 ; 0x36
-.BYTE $07 ; 0x37
-.BYTE $08 ; 0x38
-.BYTE $09 ; 0x39
-.BYTE $26 ; 0x3a
-.BYTE $35 ; 0x3b
-.BYTE $36 ; 0x3c
-.BYTE $38 ; 0x3d
-.BYTE $37 ; 0x3e
-.BYTE $39 ; 0x3f
-.BYTE $3A ; 0x40
-.BYTE $0A ; 0x41
-.BYTE $0B ; 0x42
-.BYTE $0C ; 0x43
-.BYTE $0D ; 0x44
-.BYTE $0E ; 0x45
-.BYTE $0F ; 0x46
-.BYTE $10 ; 0x47
-.BYTE $11 ; 0x48
-.BYTE $12 ; 0x49
-.BYTE $13 ; 0x4a
-.BYTE $14 ; 0x4b
-.BYTE $15 ; 0x4c
-.BYTE $16 ; 0x4d
-.BYTE $17 ; 0x4e
-.BYTE $18 ; 0x4f
-.BYTE $19 ; 0x50
-.BYTE $1A ; 0x51
-.BYTE $1B ; 0x52
-.BYTE $1C ; 0x53
-.BYTE $1D ; 0x54
-.BYTE $1E ; 0x55
-.BYTE $1F ; 0x56
-.BYTE $20 ; 0x57
-.BYTE $21 ; 0x58
-.BYTE $22 ; 0x59
-.BYTE $23 ; 0x5a
-.BYTE $3B ; 0x5b
-.BYTE $3D ; 0x5c
-.BYTE $3C ; 0x5d
-.BYTE $3E ; 0x5e
-.BYTE $2C ; 0x5f
-.BYTE $2B ; 0x60
-.BYTE $0A ; 0x61
-.BYTE $0B ; 0x62
-.BYTE $0C ; 0x63
-.BYTE $0D ; 0x64
-.BYTE $0E ; 0x65
-.BYTE $0F ; 0x66
-.BYTE $10 ; 0x67
-.BYTE $11 ; 0x68
-.BYTE $12 ; 0x69
-.BYTE $13 ; 0x6a
-.BYTE $14 ; 0x6b
-.BYTE $15 ; 0x6c
-.BYTE $16 ; 0x6d
-.BYTE $17 ; 0x6e
-.BYTE $18 ; 0x6f
-.BYTE $19 ; 0x70
-.BYTE $1A ; 0x71
-.BYTE $1B ; 0x72
-.BYTE $1C ; 0x73
-.BYTE $1D ; 0x74
-.BYTE $1E ; 0x75
-.BYTE $1F ; 0x76
-.BYTE $20 ; 0x77
-.BYTE $21 ; 0x78
-.BYTE $22 ; 0x79
-.BYTE $23 ; 0x7a
-.BYTE $3F ; 0x7b
-.BYTE $3F ; 0x7c
-.BYTE $3F ; 0x7d
-.BYTE $3F ; 0x7e
+.segment "CHR_LUT"
+chr_lut:
+.byte $ff ; 0x0 
+.byte $ff ; 0x1
+.byte $ff ; 0x2
+.byte $ff ; 0x3
+.byte $ff ; 0x4
+.byte $ff ; 0x5
+.byte $ff ; 0x6
+.byte $ff ; 0x7
+.byte $ff ; 0x8
+.byte $ff ; 0x9
+.byte $ff ; 0xa
+.byte $ff ; 0xb
+.byte $ff ; 0xc
+.byte $ff ; 0xd
+.byte $ff ; 0xe
+.byte $ff ; 0xf
+.byte $ff ; 0x10
+.byte $ff ; 0x11
+.byte $ff ; 0x12
+.byte $ff ; 0x13
+.byte $ff ; 0x14
+.byte $ff ; 0x15
+.byte $ff ; 0x16
+.byte $ff ; 0x17
+.byte $ff ; 0x18
+.byte $ff ; 0x19
+.byte $ff ; 0x1a
+.byte $ff ; 0x1b
+.byte $ff ; 0x1c
+.byte $ff ; 0x1d
+.byte $ff ; 0x1e
+.byte $ff ; 0x1f
+.byte $3f ; 0x20
+.byte $24 ; 0x21
+.byte $2d ; 0x22
+.byte $27 ; 0x23
+.byte $25 ; 0x24
+.byte $2e ; 0x25
+.byte $2f ; 0x26
+.byte $29 ; 0x27
+.byte $30 ; 0x28
+.byte $31 ; 0x29
+.byte $32 ; 0x2a
+.byte $33 ; 0x2b
+.byte $2a ; 0x2c
+.byte $27 ; 0x2d
+.byte $28 ; 0x2e
+.byte $34 ; 0x2f
+.byte $00 ; 0x30
+.byte $01 ; 0x31
+.byte $02 ; 0x32
+.byte $03 ; 0x33
+.byte $04 ; 0x34
+.byte $05 ; 0x35
+.byte $06 ; 0x36
+.byte $07 ; 0x37
+.byte $08 ; 0x38
+.byte $09 ; 0x39
+.byte $26 ; 0x3a
+.byte $35 ; 0x3b
+.byte $36 ; 0x3c
+.byte $38 ; 0x3d
+.byte $37 ; 0x3e
+.byte $39 ; 0x3f
+.byte $3a ; 0x40
+.byte $0a ; 0x41
+.byte $0b ; 0x42
+.byte $0c ; 0x43
+.byte $0d ; 0x44
+.byte $0e ; 0x45
+.byte $0f ; 0x46
+.byte $10 ; 0x47
+.byte $11 ; 0x48
+.byte $12 ; 0x49
+.byte $13 ; 0x4a
+.byte $14 ; 0x4b
+.byte $15 ; 0x4c
+.byte $16 ; 0x4d
+.byte $17 ; 0x4e
+.byte $18 ; 0x4f
+.byte $19 ; 0x50
+.byte $1a ; 0x51
+.byte $1b ; 0x52
+.byte $1c ; 0x53
+.byte $1d ; 0x54
+.byte $1e ; 0x55
+.byte $1f ; 0x56
+.byte $20 ; 0x57
+.byte $21 ; 0x58
+.byte $22 ; 0x59
+.byte $23 ; 0x5a
+.byte $3b ; 0x5b
+.byte $3d ; 0x5c
+.byte $3c ; 0x5d
+.byte $3e ; 0x5e
+.byte $2c ; 0x5f
+.byte $2b ; 0x60
+.byte $0a ; 0x61
+.byte $0b ; 0x62
+.byte $0c ; 0x63
+.byte $0d ; 0x64
+.byte $0e ; 0x65
+.byte $0f ; 0x66
+.byte $10 ; 0x67
+.byte $11 ; 0x68
+.byte $12 ; 0x69
+.byte $13 ; 0x6a
+.byte $14 ; 0x6b
+.byte $15 ; 0x6c
+.byte $16 ; 0x6d
+.byte $17 ; 0x6e
+.byte $18 ; 0x6f
+.byte $19 ; 0x70
+.byte $1a ; 0x71
+.byte $1b ; 0x72
+.byte $1c ; 0x73
+.byte $1d ; 0x74
+.byte $1e ; 0x75
+.byte $1f ; 0x76
+.byte $20 ; 0x77
+.byte $21 ; 0x78
+.byte $22 ; 0x79
+.byte $23 ; 0x7a
+.byte $3f ; 0x7b
+.byte $3f ; 0x7c
+.byte $3f ; 0x7d
+.byte $3f ; 0x7e
 
